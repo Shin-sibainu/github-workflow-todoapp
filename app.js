4dd64728379a301dc95f9c86dc78c7a0b709828f
Shin-sibainu <shincode0712@gmail.com>
2022-09-30 10:39:17 +0900

    add empty input

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 $("#addTodo").click(function () {
   const inputTodo = $("input").val();
   $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+
+  $("input").val("");
 });
 
 $(document).on("change", "input[type=checkbox]", function () {
